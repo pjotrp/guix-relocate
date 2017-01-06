461a553ba5672870b3b8ae310afcb823856e03cf
Pjotr Prins <pjotr.public01@thebird.nl>
2017-01-06 08:36:37 +0000

    Final tweaks

--- src/main.d
+++ src/main.d
@@ -2,12 +2,12 @@
 //
 // Pjotr Prins (c) 2017
 
-import std.stdio, std.getopt, std.file, std.path, std.array, std.range, std.conv, std.string;
+import std.stdio, std.getopt, std.file, std.path, std.array, std.range, std.conv, std.string, std.typecons;
 import core.sys.posix.stdlib;
 import std.algorithm.searching;
 import messages;
 
-string reduce_store_path(string fn, string prefix) {
+auto reduce_store_path(string fn, string prefix) {
   debug_info("Reduce store path "~fn);
   auto sub_paths = fn.split("/");
   auto idx = countUntil(sub_paths,"gnu");
@@ -20,11 +20,11 @@ string reduce_store_path(string fn, string prefix) {
   immutable target = prefix ~ split_path[1..$].join("-") ~ "-" ~ split_path[0] ~ "padpadpadpadpadpadpadpadpadpadpadpadpad";
   immutable from2 = "/gnu/store/"~from;
   immutable target2 = to!string(target.take(from2.length));
-  info(from2," onto ",target2);
   if (target2.length != from2.length)
     error("Paths not equally sized for "~target2); // not supposed to happen
   assert(indexOf(target2,"/gnu/store")==-1,"/gnu/store is not allowed in the target "~target2);
-  return target2;
+  info(from2," -> ",target2);
+  return tuple(target2,rest);
 }
 
 void main(string[] args) {
@@ -61,7 +61,8 @@ tar ball containing ./gnu/store/path(s).
     if (prefix[$-1]!=dirSeparator[0]) // make sure prefix ends with a separator
       prefix = prefix ~ dirSeparator;
     assert(isDir(prefix));
-    string outfn = reduce_store_path(args[1],prefix);
+    immutable res = reduce_store_path(args[1],prefix);
+    auto outfn = res[0]~"/"~res[1];
     debug_info("File = ",fn,", Size = ",buf.length,", Origin = ",origin,", Prefix = ",prefix,", Output = ",outfn);
     auto store = origin ~ "/gnu/store";
     assert(isDir(store));
@@ -70,12 +71,12 @@ tar ball containing ./gnu/store/path(s).
     // $prefix/entry-hash with the exact same size
     string[string] store_entry;
     foreach(d; dirEntries(store,SpanMode.shallow)) {
-      string target = reduce_store_path(d,prefix);
+      auto target = reduce_store_path(d,prefix)[0];
       foreach (key, value ; store_entry) {
         if (target == value)
           error("Key conflict for "~target~". Try a shorter prefix.");
       }
-      assert(!exists(target),"Directory already exists "~target);
+      // assert(exists(target),"Directory already exists "~target);
       store_entry["/gnu/store/"~baseName(d)] = target;
     }
     debug_info(store_entry);
