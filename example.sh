b4c0d3f1165e21f624ac0f2cf42d7dbea4db7460
Pjotr Prins <pjotr.public01@thebird.nl>
2017-01-07 05:52:31 +0000

    Show error status

--- example.sh
+++ example.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 rdmd src/main.d  ./gnu/store/a49zfzc6xr6g20azlpjs8sikj6v5lnkp-hello-2.10/bin/hello --prefix /gnu/tmp/hello/ --origin /home/wrk/tmp/guix-hello-2.10-x86_64 -v -d
-
+echo exit $?
